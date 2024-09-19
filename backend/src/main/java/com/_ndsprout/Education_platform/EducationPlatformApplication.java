e2694e408aacb3d5c8e152ea6b2858e348954f84
2ndsprout <2ndsprout@gmail.com>
2024-09-19 17:26:06 +0900

    스프링부트 기본설정í 적용

---
+++ backend/src/main/java/com/_ndsprout/Education_platform/EducationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com._ndsprout.Education_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationPlatformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/_ndsprout/Education_platform/EducationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com._ndsprout.Education_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
