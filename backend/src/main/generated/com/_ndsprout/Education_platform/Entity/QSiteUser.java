7a16ef6531c469731637ce151c957c2a42508f8d
hanseongeon <gkstjddjs08@naver.com>
2024-09-20 15:31:05 +0900

    yml삭제

---
+++ backend/src/main/generated/com/_ndsprout/Education_platform/Entity/QSiteUser.java
@@ -0,0 +1,51 @@
+package com._ndsprout.Education_platform.Entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QSiteUser is a Querydsl query type for SiteUser
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QSiteUser extends EntityPathBase<SiteUser> {
+
+    private static final long serialVersionUID = -1414827034L;
+
+    public static final QSiteUser siteUser = new QSiteUser("siteUser");
+
+    public final DateTimePath<java.time.LocalDateTime> createDate = createDateTime("createDate", java.time.LocalDateTime.class);
+
+    public final StringPath email = createString("email");
+
+    public final DateTimePath<java.time.LocalDateTime> modifyDate = createDateTime("modifyDate", java.time.LocalDateTime.class);
+
+    public final StringPath nickname = createString("nickname");
+
+    public final StringPath password = createString("password");
+
+    public final NumberPath<Integer> phoneNumber = createNumber("phoneNumber", Integer.class);
+
+    public final NumberPath<Integer> point = createNumber("point", Integer.class);
+
+    public final StringPath username = createString("username");
+
+    public QSiteUser(String variable) {
+        super(SiteUser.class, forVariable(variable));
+    }
+
+    public QSiteUser(Path<? extends SiteUser> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QSiteUser(PathMetadata metadata) {
+        super(SiteUser.class, metadata);
+    }
+
+}
+
---
+++ backend/src/main/java/com/_ndsprout/Education_platform/Entity/Lesson.java
@@ -0,0 +1,26 @@
+package com._ndsprout.Education_platform.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+public class Lesson {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long lessonId;
+
+    @OneToMany
+    private SiteUser siteUser;
+
+    private LocalDateTime createDate;
+
+    private LocalDateTime modifyDate;
+}
