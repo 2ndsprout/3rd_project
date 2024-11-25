9e993cf863dabc829d5a3d341ec9f8521a7a3b50
2ndsprout <2ndsprout@gmail.com>
2024-11-25 16:22:08 +0900

    siteUser 엔티티 제약조ê비밀번호í오류 수정

--- backend/src/main/java/com/_ndsprout/Education_platform/Repository/SiteUserRepository.java
+++ backend/src/main/java/com/_ndsprout/Education_platform/Repository/SiteUserRepository.java
@@ -3,8 +3,10 @@ package com._ndsprout.Education_platform.Repository;
 import com._ndsprout.Education_platform.Entity.SiteUser;
 import com._ndsprout.Education_platform.Repository.Custom.SiteUserRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface SiteUserRepository extends JpaRepository<SiteUser ,String>, SiteUserRepositoryCustom {
+@Repository
+public interface SiteUserRepository extends JpaRepository<SiteUser, String>, SiteUserRepositoryCustom {
 
-    void deleteUsername (String username);
+    void deleteByUsername(String username);
 }
