3fff8e3466563d9c4de65565abf55278775e323e
seongeon <127843337+hanseongeon@users.noreply.github.com>
2024-11-26 17:04:19 +0900

    Hse (#29)
    * 엔티티수정
    
    * 엔티티수정
    
    * 알람에 유저 추가
    
    * 서비스 수정
    
    * axios 인스톨 및 axios API 파일 추가
    
    * 유저컨트롤러
    
    * 프론트 기본 설정
    
    * DTO package make
    
    * singup
    
    * 병합충돌 해결
    
    * 회원가입 수정
    
    * 스펠ì링 체크
    
    ---------
    
    Co-authored-by: hanseongeon <gkstjddjs08@naver.com>

--- backend/src/main/java/com/_ndsprout/Education_platform/Controller/UserController.java
+++ backend/src/main/java/com/_ndsprout/Education_platform/Controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
